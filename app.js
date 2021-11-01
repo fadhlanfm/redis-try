fcef82a2caaa8f058c2aba3fc15d6b34b5f88d82
Fadhlan Fariz Makarim <fadhlanfariz95@gmail.com>
2021-11-01 15:27:10 +0700

    ide cache photos di redis

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const axios = require("axios")
 const cors = require("cors")
+const Redis = require("redis")
+const redisClient = Redis.createClient()
+
+const DEFAULT_EXPIRATION = 3600;
 
 const app = express()
 const port = process.env.port || 3000
@@ -8,11 +12,16 @@ app.use(cors())
 
 app.get("/photos", async (req, res) => {
   const albumId = req.query.albumId
+  // dicek apakah ada key photos di redis,
+  // kalo belom ada get axios
+  // kalo udah ada get dari redis
   const { data } = await axios.get(
     "http://jsonplaceholder.typicode.com/photos",
     { params : { albumId }}
   )
-
+  console.log("data", JSON.stringify(data))
+  redisClient.setex("photos", DEFAULT_EXPIRATION, JSON.stringify(data))
+  // getPhotosDariRedis()
   res.json(data)
 })
 
